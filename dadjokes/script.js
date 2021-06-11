65e1a0648fde60735dcf73ec95f0c99b36eef6a0
faishalimam38 <faishalimam38@gmail.com>
2021-06-11 16:44:42 +0530

    first commit

---
+++ dadjokes/script.js
@@ -0,0 +1,53 @@
+
+
+
+// GET https://icanhazdadjoke.com/
+
+
+const jokes = document.querySelector('#joke');
+const jokeBtn =  document.querySelector('#jokeBtn');
+
+// const generateJokes = ()=>{
+    
+//     const setHeader = {
+//         headers: {
+//             Accept:  "application/json"  
+//         }
+//     }
+
+
+//     fetch('https://icanhazdadjoke.com' , setHeader)
+//     .then((res)=>
+//         res.json()
+//     ).then((data)=>{
+//         jokes.innerHTML =  data.joke;
+//     }).catch((err)=>{
+//         console.log(err)
+//     })
+// }
+
+// async await
+const generateJokes = async ()=> {
+
+    try{
+        const setHeader = {
+            headers: {
+                Accept:  "application/json"  
+            }
+        }
+        const res =  await fetch('https://icanhazdadjoke.com', setHeader);
+        const data  = await res.json();
+        jokes.innerHTML = data.joke;
+    
+    }catch(err){
+        console.log(err)
+    }
+
+}
+
+    
+
+
+
+jokeBtn.addEventListener('click' , generateJokes);
+generateJokes();
